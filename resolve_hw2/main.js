dc4693d56872be81629ef202b2f180b4c2659872
MarynaStan <vasylchenko84@gmail.com>
2022-11-03 17:42:59 +0200

    changed

--- resolve_hw2/main.js
+++ resolve_hw2/main.js
@@ -1,42 +1,112 @@
 //1.Створити масив, наповнити його 10 елементами
 // будь-якого типу, вивести кожен елемент в консоль
-const arr1=[1,2,5,"text",true, 100, -55,0, "hello",3];
+const arr=[1,2,5,"text",true, 100, -55,0, "hello",3];
 
-console.log("arr1 = ", arr1);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
+
 console.log("================");
 
 //2.Створити 3 об'єкти
 // які описують книги.
 // Поля об'єкту : title ,pageCount, genre.;
 
-const bookTitle = {title: "Harry Potter"};
-console.log("bookTitle = ", bookTitle);
+let book1 ={
+    title: "Harry Potter",
+    pageCount: 250,
+    genre: "fantasy"
+}
+console.log("title = ", book1.title);
+console.log("pageCount = ", book1.pageCount);
+console.log("genre = ", book1.genre);
 
-const bookPageCount = { pageCount: 250};
-console.log("bookPageCount = ", bookPageCount );
+let book2 ={
+    title: "Romeo and Juliet",
+    pageCount: 470,
+    genre: "roman"
+}
+console.log("title = ", book2.title);
+console.log("pageCount = ", book2.pageCount);
+console.log("genre = ", book2.genre);
 
-const bookGenre = { genre: "fantasy"};
-console.log("bookGenre = ", bookGenre);
+let book3 ={
+    title: "Hobbits",
+    pageCount: 520,
+    genre: "fantasy"
+}
+console.log("title = ", book3.title);
+console.log("pageCount = ", book3.pageCount);
+console.log("genre = ", book3.genre);
 console.log("================");
 
 
 //3.Створити 3 об'єкти які описують книги.
 // Поля об'єкту : title ,pageCount, genre, authors.
 //  Поле "автори" - масив. Кожен автор має поля name,age
-const bookTitle2 = {title: "Harry Potter"};
-console.log("bookTitle2 = ", bookTitle2);
-
-const bookPageCount2 = { pageCount: 250};
-console.log("bookPageCount2 = ", bookPageCount2 );
-
-const bookGenre2 = { genre: "fantasy"};
-console.log("bookGenre2 = ", bookGenre2);
-const bookAuthor2 = { authors: [
+book1 ={
+    title: "Harry Potter",
+    pageCount: 250,
+    genre: "fantasy",
+    authors: [
         {name: "Name1", age: 29},
         {name: "Name2", age: 30},
         {name: "Name3", age: 35}
-    ]};
-console.log("bookAuthor2 = ", bookAuthor2);
+    ]
+}
+
+console.log("title = ", book1.title);
+console.log("pageCount = ", book1.pageCount);
+console.log("genre = ", book1.genre);
+console.log("authors1 = ", book1.authors[0]);
+console.log("authors2 = ", book1.authors[1]);
+console.log("authors3 = ", book1.authors[2]);
+
+book2 ={
+    title: "Romeo and Juliet",
+    pageCount: 470,
+    genre: "roman",
+    authors: [
+        {name: "Name4", age: 45},
+        {name: "Name5", age: 39},
+        {name: "Name6", age: 52}
+    ]
+}
+
+console.log("title = ", book2.title);
+console.log("pageCount = ", book2.pageCount);
+console.log("genre = ", book2.genre);
+console.log("authors1 = ", book2.authors[0]);
+console.log("authors2 = ", book2.authors[1]);
+console.log("authors3 = ", book2.authors[2]);
+
+book3 ={
+    title: "Hobbits",
+    pageCount: 520,
+    genre: "fantasy",
+    authors: [
+        {name: "Name7", age: 35},
+        {name: "Name8", age: 42},
+        {name: "Name9", age: 32}
+    ]
+}
+
+console.log("title = ", book3.title);
+console.log("pageCount = ", book3.pageCount);
+console.log("genre = ", book3.genre);
+console.log("authors1 = ", book3.authors[0]);
+console.log("authors2 = ", book3.authors[1]);
+console.log("authors3 = ", book3.authors[2]);
+
+
+
 console.log("================");
 
 //4.Створити масив з 10 об'єктами які описують сутніть "користувач".
